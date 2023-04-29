fd8e18278718af524f8af5b76cb1051d8a53335a
AnchorDev <01.jakub.kotwica@gmail.com>
2023-04-29 22:32:32 +0200

    klasa board, zmiany w mainie, dodanie liter i cyfr

--- Chess/src/application/Main.java
+++ Chess/src/application/Main.java
@@ -22,42 +22,45 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 
 
 public class Main extends Application {
 	
-	//wielkosc szachownicy i potrzebne tablice koordynatow i bierek
-	private static int boardSize = 8;
-	private static int[] coordinates;
 	private Pieces[] piece = new Pieces[32];
+	Board board = new Board();
 	
-	//wybor szachownicy
-	private static boolean kuba = true;
-	private static boolean radek = false;
+	public enum Choice{kuba,radek};
 	
 	//dodanie sceny i grup (taka tablica elementow, dodaje sie jako argument do paneli zeby sie wyswietlaly)
 	private static Scene scene;
-	private static Group gridGroup;
-	private static Group pieceGroup;
+
+
 	
-	
+	/*
 	//do obrazkow, sprawdzam czy dziala
 	private static InputStream stream;
     private static Image image;
     private static ImageView imageView;
-	
+	*/
 	
 	@Override
 	public void start(Stage stage) {
 		
-		gridGroup = new Group();
-		pieceGroup = new Group();
-		Pane pane = new Pane(gridGroup, pieceGroup);
+		
+		
+		
+		Pane pane = new Pane(board.gridGroup, board.pieceGroup, board.textGroup);
 		scene = new Scene(pane, 850, 850);
-
+		
 		addPieces();
-		drawBoard(kuba);
+		
+		board.setChoice(Choice.kuba);
+		board.drawBoard();
+
 		stage.setScene(scene);
         stage.setTitle("Czachy");
         stage.setResizable(false);
@@ -67,74 +70,8 @@ public class Main extends Application {
 
 	}
 	
-	//rysuje szachownice, trzeba dodac rysowanie pionkow
-	private void drawBoard(boolean board)
-	{
-		int i,j,wcolor = 0;
-		
-		if(board == kuba)
-		{
-			for(i = 0; i < boardSize; i++)
-			{
-				for(j = 0; j < boardSize; j++)
-				{
-					Rectangle r = new Rectangle();
-					
-					coordinates = setCoordinates(i,j);
-					
-					r.setX(coordinates[0]);
-	                r.setY(coordinates[1]);
-	                r.setWidth(100);
-	                r.setHeight(100);
-	                r.setStroke(Color.BLACK);
-	                
-	                if(wcolor % 2 == 0)	r.setFill(Color.WHITE);
-	                else	r.setFill(Color.BLACK);
-					
-					gridGroup.getChildren().add(r);
-					
-					wcolor++;
-				}
-				wcolor++;
-			}
-		}
-		
-		else if(board == radek)
-		{
-			for(i = 0; i < boardSize; i++)
-			{
-				for(j = 0; j < boardSize; j++)
-				{
-					Rectangle r = new Rectangle();
-					
-					coordinates = setCoordinates(i,j);
-					
-					r.setX(coordinates[0]);
-	                r.setY(coordinates[1]);
-	                r.setWidth(100);
-	                r.setHeight(100);
-	                r.setStroke(Color.BLACK);
-	                
-	                if(wcolor % 2 == 0)	r.setFill(Color.WHITE);
-	                else	r.setFill(Color.rgb(14, 156, 12));
-					
-					gridGroup.getChildren().add(r);
-					
-					wcolor++;
-				}
-				wcolor++;
-			}
-			
-			for(i = 0; i < piece.length; i++)
-			{
-				setImg(piece[i].getFilename(), piece[i].getX(), piece[i].getY());
-			}
-		}
-		
-		
-		
-	}
-	
+
+	/*
 	//ustawia zdjecia figur
 	private void setImg(String filename, int i, int j)
 	{
@@ -152,29 +89,15 @@ public class Main extends Application {
 		
 		
 	}
-
-	
-	//przypisuje koordynaty poszczegolnych kafelkow szachownicy
-	private int[] setCoordinates(int x, int y)
-	{
-		
-		int[] xy = new int[2];
-		
-		xy[0] = x*100+25;
-		xy[1] = y*100+25;
-		
-		return xy;
-		
-	}
-	
+*/
 	
 	//dodaje bierki do listy elementow
 	private void addPieces()
 	{
 		int count = 0;
-		for(int i = 0; i < boardSize; i++)
+		for(int i = 0; i < board.boardSize; i++)
 		{
-			for(int j = 0; j < boardSize; j++)
+			for(int j = 0; j < board.boardSize; j++)
 			{
 	               if (j == 0) { // 8th rank
 	                    if (i == 0 || i == 7) {piece[count] = new Pieces(i, j, "black", "rook", "D:\\eclipse prace\\szachy\\src\\Pieces\\bRook");
